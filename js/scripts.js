575480fe226e6c33a42924f0905787ab15a60320
Chase Grijalva and Olena Kuchko <olena@>
2017-04-24 16:00:03 -0700

    add concat

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,70 @@
 $(document).ready(function(){
-  var funcFirst = function (){
-    var getAnswer = function (){
-    return prompt("Enter a sentence");
-    };
-    var sentence = getAnswer();
+  var getAnswer = function (){
+  return prompt("Enter a sentence");
+  };
+  var sentence = getAnswer();
+  //console.log(getAnswer());
+  //console.log(sentence);
 
-    alert (sentence);
-    console.log(sentence);
-
-    var str = sentence[0];
-    str += sentence[sentence.length-1];
-
-    var upperStr = str.toUpperCase();
-    alert(upperStr);
-    return upperStr;
+  var getLetters = function(){
+    var letters = sentence[0] + sentence[sentence.length-1];
+    return letters.toUpperCase();
+    //console.log(letters);
+  };
+  var reverseLetters = function(){
+    var splitString = getLetters().split("");
+    var reverseStr = splitString.reverse();
+    var joinStr = reverseStr.join("");
+    //console.log(joinStr);
+    return joinStr;
   };
 
- var funcSec =function(upperStr){
-  var splitString = upperStr.split("");
-  var reverseStr = splitString.reverse();
-  var joinStr = reverseStr.join("");
 
-  console.log(joinStr);
-  };
-    var funcThird = function(){
-    var upperS = funcFirst();
-    funcSec(upperS);
-  }
-  funcThird();
+
+ var conCat = function(){
+   console.log(sentence.concat(reverseLetters()));
+ };
+ conCat();
+  // var getAnswer = function (){
+  // return prompt("Enter a sentence");
+  // };
+  //
+  // var sentence = getAnswer();
+  //
+  // var funcFirst = function (){
+  //   console.log(sentence);
+  //
+  //   var str = sentence[0];
+  //   str += sentence[sentence.length-1];
+  //
+  //   var upperStr = str.toUpperCase();
+  //   return upperStr;
+  // };
+  //
+  // var funcSec = function(){
+  //   var splitString = funcFirst().split("");
+  //   var reverseStr = splitString.reverse();
+  //   var joinStr = reverseStr.join("");
+  //   console.log(joinStr);
+  // };
+  //
+  //   // funcFirst();
+  //   //
+  //   // funcSec();
+  //
+  // var funcThird = function(){
+  //
+  //   funcSec(funcFirst());
+  // };
+  //
+  //
+  // var funcFour = function(){
+  //   funcThird();
+  //   console.log(sentence);
+  //   console.log(sentence.concat(funcSec()));
+  // };
+  //
+  // funcFour();
+
+
 });
