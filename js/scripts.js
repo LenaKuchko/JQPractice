3cc2a16948de73c9bede236fe35443933b860bad
Chase Grijalva and Olena Kuchko <olena@>
2017-04-24 14:34:58 -0700

    added functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,31 @@
 $(document).ready(function(){
-  var getAnswer = function (){
-  return prompt("Enter a sentence");
+  var funcFirst = function (){
+    var getAnswer = function (){
+    return prompt("Enter a sentence");
+    };
+    var sentence = getAnswer();
+
+    alert (sentence);
+    console.log(sentence);
+
+    var str = sentence[0];
+    str += sentence[sentence.length-1];
+
+    var upperStr = str.toUpperCase();
+    alert(upperStr);
+    return upperStr;
   };
-  var sentence = getAnswer();
 
-  alert (sentence);
-  console.log(sentence);
-
-  var str = sentence[0];
-  str += sentence[sentence.length-1];
-
-  var upperStr = str.toUpperCase();
-  alert(upperStr);
+ var funcSec =function(upperStr){
   var splitString = upperStr.split("");
   var reverseStr = splitString.reverse();
   var joinStr = reverseStr.join("");
+
   console.log(joinStr);
+  };
+    var funcThird = function(){
+    var upperS = funcFirst();
+    funcSec(upperS);
+  }
+  funcThird();
 });
